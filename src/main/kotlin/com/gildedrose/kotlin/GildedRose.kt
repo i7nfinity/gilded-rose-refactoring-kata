3905bb3b4a0304404b577e62af3ca04644da974d
apetrov-08 <9119123210@mail.ru>
2025-09-25 17:56:28 +0200

    bullet points refactor

--- src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
@@ -4,61 +4,67 @@ class GildedRose(val items: List<Item>) {
 
     // TODO: большая вложенность
     // TODO: Item - ленивый класс
+
+    val AGED_BRIE = "Aged Brie"
+    val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+    val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    val MAX_QUALITY = 50
+    val BACKSTAGE_BRIE_DAYS_THRESHOLD_LATER = 11
+    val BACKSTAGE_BRIE_DAYS_THRESHOLD_LATEST = 6
+
     fun updateQuality() {
         for (i in items.indices) {
+            val item = items[i]
             // TODO: дублирующийся код
-            // TODO: magic strings
-            // TODO: items[i] - не вынесено в переменную в начале цикла
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        // TODO: изменение значений не напрямую, а через другой класс
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
+            if (item.name != AGED_BRIE && item.name != BACKSTAGE) {
+                changeQualityForSulfuras(item)
             } else {
-                // TODO: magic numbers
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < MAX_QUALITY) {
+                    changeQuality(item, 1)
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                // TODO: вынести изменения значений в методы (дублирующиеся изменения quality и sellIn)
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+                    if (item.name == BACKSTAGE) {
+                        changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATER)
+                        changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATEST)
                     }
                 }
             }
  
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != SULFURAS) {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
+            if (item.sellIn < 0) {
+                if (item.name != AGED_BRIE) {
+                    if (item.name != BACKSTAGE) {
+                        changeQualityForSulfuras(item)
                     } else {
-                        // TODO: сделать нормальное обнуление
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = 0
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < MAX_QUALITY) {
+                        changeQuality(item, 1)
                     }
                 }
+            }
+        }
+    }
+    
+    private fun changeQuality(item: Item, diff: Int) {
+        item.quality += diff
+    }
+
+    private fun changeBackstageBrieQuality(item: Item, threshold: Int) {
+        if (item.sellIn < threshold) {
+            if (item.quality < MAX_QUALITY) {
+                changeQuality(item, 1)
+            }
+        }
+    }
+
+    private fun changeQualityForSulfuras(item: Item) {
+        if (item.quality > 0) {
+            if (item.name != SULFURAS) {
+                changeQuality(item, -1)
             }
         }
     }
