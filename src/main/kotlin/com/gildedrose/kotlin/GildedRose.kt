7da2df8b0c28929e65ffd8fb78f4bf0b055a9acd
tsedrikns <cedrikns@gmail.com>
2025-09-25 17:57:54 +0300

    add TODO with code smells

--- src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
@@ -2,21 +2,29 @@ package com.gildedrose.kotlin
 
 class GildedRose(val items: List<Item>) {
 
+    // TODO: большая вложенность
+    // TODO: Item - ленивый класс
     fun updateQuality() {
         for (i in items.indices) {
+            // TODO: дублирующийся код
+            // TODO: magic strings
+            // TODO: items[i] - не вынесено в переменную в начале цикла
             if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                 if (items[i].quality > 0) {
                     if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                        // TODO: изменение значений не напрямую, а через другой класс
                         items[i].quality = items[i].quality - 1
                     }
                 }
             } else {
+                // TODO: magic numbers
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
+                                // TODO: вынести изменения значений в методы (дублирующиеся изменения quality и sellIn)
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -43,6 +51,7 @@ class GildedRose(val items: List<Item>) {
                             }
                         }
                     } else {
+                        // TODO: сделать нормальное обнуление
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
