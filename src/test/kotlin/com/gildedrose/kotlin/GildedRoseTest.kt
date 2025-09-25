26c2bce437ff7117977bdd2faa733f345803b5a8
akortunov <truba191@yandex.ru>
2025-09-25 16:57:55 +0300

    tests

--- src/test/kotlin/com/gildedrose/kotlin/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/kotlin/GildedRoseTest.kt
@@ -10,8 +10,67 @@ internal class GildedRoseTest {
         val items = listOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
+    }
 
+    @Test
+    fun dexterity_Vest() {
+        val items = listOf(Item("Dexterity Vest", 10, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Dexterity Vest", app.items[0].name)
+        assertEquals(9, app.items[0].sellIn)
+        assertEquals(19, app.items[0].quality)
+    }
+
+    @Test
+    fun TAFKAL80ETC_15_20() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name)
+        assertEquals(9, app.items[0].sellIn)
+        assertEquals(22, app.items[0].quality)
+    }
+
+    @Test
+    fun TAFKAL80ETC_5_20() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name)
+        assertEquals(4, app.items[0].sellIn)
+        assertEquals(23, app.items[0].quality)
+    }
+
+    @Test
+    fun test_5_20() {
+        val items = listOf(Item("test", -1, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("test", app.items[0].name)
+        assertEquals(-2, app.items[0].sellIn)
+        assertEquals(18, app.items[0].quality)
+    }
+
+    @Test
+    fun test2_5_20() {
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", -1, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name)
+        assertEquals(-2, app.items[0].sellIn)
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun test3_5_20() {
+        val items = listOf(Item("Aged Brie", -1, 20))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals("Aged Brie", app.items[0].name)
+        assertEquals(-2, app.items[0].sellIn)
+        assertEquals(22, app.items[0].quality)
     }
 
 }
