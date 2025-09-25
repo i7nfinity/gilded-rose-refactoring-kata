8e6e7b93fc714c9504f00d22752fd3c9e60866cd
ebashunov-yandex <ebashunov@yandex-team.ru>
2025-09-25 18:54:46 +0300

    Update Item.kt

--- src/main/kotlin/com/gildedrose/kotlin/Item.kt
+++ src/main/kotlin/com/gildedrose/kotlin/Item.kt
@@ -6,4 +6,24 @@ open class Item(
     var quality: Int,
 ) {
     override fun toString(): String = "$name, $sellIn, $quality"
+
+    fun decrementQuality() {
+        this.quality = this.quality.dec()
+    }
+
+    fun incrementQuality() {
+        this.quality = this.quality.inc()
+    }
+
+    fun incrementQualityIfPossible() {
+        if (!hasMaximalQuality()) {
+            incrementQuality()
+        }
+    }
+
+    fun hasMaximalQuality(): Boolean = this.quality >= MAX_ITEM_QUALITY
+
+    companion object {
+        const val MAX_ITEM_QUALITY = 50
+    }
 }
