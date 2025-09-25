de61aa33f80239902c085484cfadaed8062003ad
tsedrikns <cedrikns@gmail.com>
2025-09-25 19:21:40 +0300

    super refactor

--- src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/kotlin/GildedRose.kt
@@ -1,13 +1,7 @@
 package com.gildedrose.kotlin
 
 class GildedRose(val items: List<Item>) {
-
-    // TODO: большая вложенность
-    // TODO: Item - ленивый класс
-
-    val AGED_BRIE = "Aged Brie"
-    val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
-    val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    
     val MAX_QUALITY = 50
     val BACKSTAGE_BRIE_DAYS_THRESHOLD_LATER = 11
     val BACKSTAGE_BRIE_DAYS_THRESHOLD_LATEST = 6
@@ -15,42 +9,22 @@ class GildedRose(val items: List<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             val item = items[i]
-            // TODO: дублирующийся код
-            if (item.name != AGED_BRIE && item.name != BACKSTAGE) {
-                changeQualityForSulfuras(item)
-            } else {
-                if (item.quality < MAX_QUALITY) {
-                    changeQuality(item, 1)
 
-                    if (item.name == BACKSTAGE) {
-                        changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATER)
-                        changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATEST)
-                    }
-                }
-            }
- 
-            if (item.name != SULFURAS) {
-                item.sellIn = item.sellIn - 1
-            }
-
-            if (item.sellIn < 0) {
-                if (item.name != AGED_BRIE) {
-                    if (item.name != BACKSTAGE) {
-                        changeQualityForSulfuras(item)
-                    } else {
-                        item.quality = 0
-                    }
-                } else {
-                    if (item.quality < MAX_QUALITY) {
-                        changeQuality(item, 1)
-                    }
-                }
-            }
+            changeQuality(item)
+            changeSellIn(item)
+            processNegativeSellIn(item)
         }
     }
     
     private fun changeQuality(item: Item, diff: Int) {
         item.quality += diff
+        if (item.quality < 0) {
+            item.quality = 0
+        }
+
+        if (item.quality > MAX_QUALITY) {
+            item.quality = MAX_QUALITY
+        }
     }
 
     private fun changeBackstageBrieQuality(item: Item, threshold: Int) {
@@ -61,13 +35,46 @@ class GildedRose(val items: List<Item>) {
         }
     }
 
-    private fun changeQualityForSulfuras(item: Item) {
-        if (item.quality > 0) {
-            if (item.name != SULFURAS) {
-                changeQuality(item, -1)
+    private fun changeQuality(item: Item) {
+        when(item.type) {
+            ItemType.BRIE -> {
+                changeQuality(item, 1)
             }
+            ItemType.BACKSTAGE -> {
+                changeQuality(item, 1)
+                changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATER)
+                changeBackstageBrieQuality(item, BACKSTAGE_BRIE_DAYS_THRESHOLD_LATEST)
+            }
+            ItemType.SULFURAS -> Unit
+            ItemType.COMMON -> changeQuality(item, -1)
         }
     }
 
+    private fun changeSellIn(item: Item) {
+        when(item.type) {
+            ItemType.BRIE,
+            ItemType.BACKSTAGE,
+            ItemType.COMMON -> item.sellIn -= 1
+            ItemType.SULFURAS -> Unit
+        }
+    }
+
+    // TODO: rename
+    private fun processNegativeSellIn(item: Item) {
+        if (item.sellIn >= 0) {
+            return
+        }
+        when(item.type) {
+            ItemType.BRIE -> {
+                if (item.quality < MAX_QUALITY) {
+                    changeQuality(item, 1)
+                }
+            }
+            ItemType.BACKSTAGE -> item.quality = 0
+            ItemType.SULFURAS -> Unit
+            ItemType.COMMON ->  changeQuality(item, -1)
+        }
+
+    }
 }
 
--- src/main/kotlin/com/gildedrose/kotlin/Item.kt
+++ src/main/kotlin/com/gildedrose/kotlin/Item.kt
@@ -5,5 +5,19 @@ open class Item(
     var sellIn: Int,
     var quality: Int,
 ) {
+    val type = when {
+        name == "Aged Brie" -> ItemType.BRIE
+        name == "Backstage passes to a TAFKAL80ETC concert" -> ItemType.BACKSTAGE
+        name == "Sulfuras, Hand of Ragnaros" -> ItemType.SULFURAS
+        else -> ItemType.COMMON
+    }
+
     override fun toString(): String = "$name, $sellIn, $quality"
+}
+
+enum class ItemType {
+    BRIE,
+    BACKSTAGE,
+    SULFURAS,
+    COMMON,
 }
